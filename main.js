35126140dbee07dede744b756ff08dfa70ecfb4d
HoWingYip <sploooosh884@gmail.com>
2018-08-19 11:29:17 +0800

    Added scripts and styles files, stylized textarea

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ function createWindow () {
   // and load the index.html of the app.
   win.loadFile('index.html')
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+  // Open the DevTools (disabled by me).
+  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
